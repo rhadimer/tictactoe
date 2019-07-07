f2658a1718b31341c27d9668c1aa051363830971
Rhadimer Antigua Sánchez <rhadimer_ants@hotmail.com>
2019-07-07 11:32:42 -0400

    share runtime off
    share runtime off

--- GridExample/GridExample.Android/MainActivity.cs
+++ GridExample/GridExample.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace GridExample.Droid
 {
-    [Activity(Label = "GridExample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TicTacToe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
